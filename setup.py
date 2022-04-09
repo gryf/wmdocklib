fff744ae32da491201d31616397da2496fd2d676
gryf <git@vimja.com>
2022-04-09 11:35:55 +0200

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,23 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from distutils.core import Extension
+from distutils.core import setup
 
 # Set these so they match your system.
-XLibDir = '/usr/X11R6/lib'
-XIncludes = '/usr/X11R6/include'
+XLIBDIR = '/usr/X11R6/lib'
+XINCLUDES = '/usr/X11R6/include'
 
-from distutils.core import setup, Extension
 
-module1 = Extension('wmdocklib.pywmgeneral',
-                    libraries = ['Xpm', 'Xext', 'X11'],
-                    library_dirs = [XLibDir],
-                    include_dirs = [XIncludes],
-                    sources = ['wmdocklib/pywmgeneral.c'])
+MODULE1 = Extension('wmdocklib.pywmgeneral',
+                    libraries=['Xpm', 'Xext', 'X11'],
+                    library_dirs=[XLIBDIR],
+                    include_dirs=[XINCLUDES],
+                    sources=['wmdocklib/pywmgeneral.c'])
 
 setup(name="pywmdockapps",
-      version = "$Revision$"[11:-2],
-
-      description='''
-         read the whole story at http://pywmdockapps.sourceforge.net/''',
-      
+      version="1.23",
+      description="Library for creating dockapps for Window Maker/AfterStep",
       author="Kristoffer Erlandsson & al.",
       author_email="mfrasca@zonnet.nl",
       url="http://pywmdockapps.sourceforge.net",
       license="(L)GPL",
-      packages=['wmdocklib',
-                ],
-      scripts=['examples/pywmdatetime.py',
-               'examples/pywmhdmon.py',
-               'examples/pywmseti.py',
-               'examples/pywmsysmon.py',
-               'examples/pywmphoto.py',
-               'examples/pywmwet.py',
-               'examples/pywmradio.py',
-               'examples/pywmgeneric.py'],
-      package_data={'wmdocklib': ['*.xpm']},
-      ext_modules = [module1])
+      packages=['wmdocklib'],
+      ext_modules=[MODULE1])
