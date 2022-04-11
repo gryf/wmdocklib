efb6f8f76454a71d6a54d666608130bae962de28
gryf <git@vimja.com>
2022-04-11 18:17:12 +0200

    Added simple DockApp base class

--- wmdocklib/__init__.py
+++ wmdocklib/__init__.py
@@ -1,12 +1,24 @@
-"""\
-wmdocklib is a python module that will help you develop WindowMaker dockapps
-in python.  It is mostly a wrapper around the functions from the popular
-wmgeneral.c, but some new functions are added too.
+import sys
 
-to get help about a contained package, try:
-help(wmdocklib.<name>)
-"""
-from pywmgeneral import *
-from pywmhelpers import *
+from wmdocklib import helpers
 
-__all__ = ['wmoo']
+
+class DockApp:
+    width = 64
+    height = 64
+    x_offset = 4
+    y_offset = 4
+    palette = {"1": "black",
+               "2": "white"}
+
+    def __init__(self, args=None):
+        self._args = args
+        self._charset_start = None
+        self._charset_width = None
+
+    def check_for_events(self):
+        event = helpers.get_event()
+        while event is not None:
+            if event['type'] == 'destroynotify':
+                sys.exit(0)
+            event = helpers.get_event()
